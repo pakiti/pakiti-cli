35e433d554ce468da5ad9ffaad34cb739f28b883
Alpha Olomi <alphaolomi@gmail.com>
2022-11-13 15:44:46 +0300

    Update NewCommand.php

--- app/Commands/NewCommand.php
+++ app/Commands/NewCommand.php
@@ -12,7 +12,7 @@ class NewCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'new {name=Artisan}';
+    protected $signature = 'new {type=php-package}';
 
     /**
      * The description of the command.
